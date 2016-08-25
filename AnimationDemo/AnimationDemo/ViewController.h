fdb7df070512a98dda74a8ab899779fd24bc0c4f
jackytianhappy <1142899950@qq.com>
2016-08-25 17:26:38 +0800

    初始化Animation动画效果

---
+++ AnimationDemo/AnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by Jacky on 16/8/25.
+//  Copyright © 2016年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
